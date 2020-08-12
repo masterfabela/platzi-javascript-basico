8891a15156206a043de895bb1483b4118bdf76e0
Jose Ramón Vieites <fabelaretorcida@gmail.com>
2020-08-12 20:37:25 +0200

    scope

---
+++ scope.js
@@ -0,0 +1,12 @@
+var miNombre = 'diego';
+
+function nombre() {
+  var apellido = 'vieites';
+  console.log(miNombre + ' ' + apellido);
+}
+
+console.log(miNombre);
+
+nombre();
+
+console.log(apellido);

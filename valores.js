2506c26cf7342e6f57fa0552bff8122cec24c850
Jose Ramón Vieites <fabelaretorcida@gmail.com>
2020-08-12 20:25:27 +0200

    Funciones

---
+++ valores.js
@@ -0,0 +1,15 @@
+40;
+
+('Diego de granda');
+
+true;
+false;
+
+null;
+undefined;
+
+[1, 2, 3, 4, 5, 6];
+
+{
+  nombre: 'Diego';
+}
---
+++ variables.js
@@ -0,0 +1,18 @@
+var nombre = 'Oscar';
+
+// Declarar
+var edad;
+
+edad;
+// Inicializar
+edad = 18;
+edad;
+
+var elementos = ['Computadora', 'Celular'];
+
+var persona = {
+  nombre: 'Diego',
+  edad: 30,
+};
+
+persona;

8fd8adf2d9e7e0d9ff921e501c010760c45e753d
Jose Ramón Vieites <fabelaretorcida@gmail.com>
2020-08-13 21:56:25 +0200

    cohercion

---
+++ cohercion.js
@@ -0,0 +1,16 @@
+var variable = 4 + '7';
+console.log(variable);
+console.log(typeof variable);
+
+var b = 4 * '7';
+console.log(typeof b);
+
+a = 20;
+b = a + '';
+console.log(typeof b);
+
+var c = String(a);
+console.log(typeof c);
+
+var d = Number(c);
+console.log(typeof d);

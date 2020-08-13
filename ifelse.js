537aa2788c3aaca8e9fe44c229305ca94ef86208
Jose Ramón Vieites <fabelaretorcida@gmail.com>
2020-08-13 22:48:39 +0200

    if else

---
+++ ifelse.js
@@ -0,0 +1,14 @@
+var edad = 23;
+
+if (edad === 18) {
+  console.log('Puedes Votar, Sera tu primerito dia');
+} else if (edad > 18) {
+  console.log('Puedes Votar de nuevo');
+} else {
+  console.log('No puedes votar');
+}
+
+var numero = 1;
+var resultado = numero === 1 ? 'Si, soy un uno' : 'No, no soy un uno';
+
+console.log(resultado);

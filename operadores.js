433ac9c3bac9c50ae66cbb88c87df70fc562a29f
Jose Ramon Vieites <jose.vieites@altia.es>
2021-03-10 01:36:20 +0100

    Clase 11 redoing

--- operadores.js
+++ operadores.js
@@ -1,8 +1,23 @@
 var a = 3 + 2;
-
+var b;
+a = 1;
 console.log(a);
 
 var edad = 40;
 edad += 2;
 console.log(edad);
- 
+
+console.log('Diego' + ' De granda');
+console.log(3 == '3');
+console.log(3 === '3');
+console.log(5 < 3);
+console.log(5 > 3);
+console.log(5 <= 6);
+console.log(5 >= 6);
+console.log(Boolean(a && b));
+console.log(Boolean(a || b));
+console.log(a && b);
+console.log(a || b);
+
+var edad = 40;
+console.log((edad += 2));

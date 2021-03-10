a9cc7c59466971fca5867b4427752c3d9b959a83
Jose Ramon Vieites <jose.vieites@altia.es>
2021-03-10 01:11:20 +0100

    Clase 10 remaster

--- trultyFalsy.js
+++ trultyFalsy.js
@@ -1,17 +1,31 @@
 var a = Boolean('true');
 
+// Falsos
+console.log(Boolean(undefined));
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(null));
+
+// Verdadeiros
+console.log(Boolean([]));
+console.log(Boolean('a'));
+console.log(Boolean(1));
+console.log(Boolean({}));
+console.log(Boolean(() => {}));
+console.log(Boolean(function () {}));
+
 console.log(a);
 
 var edad = 0;
 var dni = '';
 
 var persona = {
-  dni: dni || 'Sin asignar',
-  edad: edad || 18,
+	dni: dni || 'Sin asignar',
+	edad: edad || 18,
 };
 
 if (edad) {
-  console.log('edad definida');
+	console.log('edad definida');
 }
 
 console.log(persona);
